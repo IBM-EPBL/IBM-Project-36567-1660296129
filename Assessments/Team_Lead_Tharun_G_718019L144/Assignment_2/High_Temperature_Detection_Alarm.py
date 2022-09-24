76214852af7fa59c6eb1fe4086f4af8be6debef7
Tharun G <65439029+TharunNo1@users.noreply.github.com>
2022-09-24 17:41:03 +0530

    Assignment 2 uploaded

---
+++ Assessments/Team_Lead_Tharun_G_718019L144/Assignment_2/High_Temperature_Detection_Alarm.py
@@ -0,0 +1,14 @@
+import random as src
+while True:
+    temperature = src.randint(10,100)
+    humidity = src.randint(10,100)
+    if humidity>35 and temperature>60:
+        print("Temperature:",temperature,"degree celsius")
+        print("Humidity:",humidity,"grams/(meter^3)")
+        print("ALARM ON")
+    else:
+        print("Temperature:",temperature,"degree celsius")
+        print("Humidity:",humidity,"grams/(meter^3)")
+        print("ALARM OFF")
+    print("\n","*"*30,"\n",sep="")
+    
